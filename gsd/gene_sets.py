fbddb0f41d3d085c4b84d45e1bd9b402192a0e58
Rudolf Biczok <rcbiczok@gmail.com>
2018-12-25 18:27:28 +0100

    Rename attribute containing the entrez genes

--- gsd/gene_sets.py
+++ gsd/gene_sets.py
@@ -8,15 +8,15 @@ class GeneSet:
                  external_source: str,
                  summary: str,
                  calculated: bool,
-                 entregene_ids: Set[float],
+                 entrez_gene_ids: Set[float],
                  gene_symbols: Set[str]):
         self.name = name
         self.external_id = external_id
         self.external_source = external_source
         self.summary = summary
         self.calculated = calculated
-        self.entregene_ids = entregene_ids
+        self.entrez_gene_ids = entrez_gene_ids
         self.gene_symbols = gene_symbols
 
     def __repr__(self):
-        return "<GeneSet(name='%s', n_external_id='%s')>" % (self.name, len(self.external_id))
+        return "<GeneSet(name='%s', n_entrez_gene_ids='%s')>" % (self.name, len(self.entrez_gene_ids))
